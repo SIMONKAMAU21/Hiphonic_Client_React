31293eec53309502d3e6ce2f2e19a98658450dba
Stephen Ondieki <stevehotcodes@gmail.com>
2024-02-25 15:16:06 +0300

    (chore) & (fix) added the navigation for the events and fixed the issue on the styling in the events

--- src/layouts/Content/Content.jsx
+++ src/layouts/Content/Content.jsx
@@ -6,6 +6,8 @@ import { Route } from 'react-router-dom'
 import Friends from '../../pages/Friends/Friends'
 import Videos from '../../pages/videos/Videos'
 import Groups from '../../pages/Groups/Groups'
+import Photos from '../../pages/Photos/Photos'
+import Events from '../../pages/Events/Events'
 
 
 
@@ -17,9 +19,9 @@ const Content=()=>{
             <Routes>
                  {/* <Route path='/profile' exact  element={<Profil/>} ></Route>
                  <Route path='/friends' element={<F>} ></Route>
-                 <Route path='/timeline'element={<Timeline/>} ></Route>
-                 <Route path='/photos' element={<Ph>}></Route>
-    <Route path='/events' element={<Events/>}></Route>*/}
+    <Route path='/timeline'element={<Timeline/>} ></Route>*/}
+     <Route path='/events' element={<Events/>}></Route>
+    <Route path='/photos' element={<Photos/>}></Route>
     <Route path='/groups' element={<Groups/>}></Route>
                  <Route path='/videos' element={<Videos/>}></Route>  
                  <Route path='/friends' element={<Friends/>}></Route>
