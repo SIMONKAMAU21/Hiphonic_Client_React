1248375436dd3ce9b55847b75515682a93a8643a
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-02-28 05:07:55 -0800

    worked on correcting timeline

--- src/components/PostDiv/PostDiv.jsx
+++ src/components/PostDiv/PostDiv.jsx
@@ -2,55 +2,64 @@ import React from 'react'
 import '../../components/PostDiv/PostDiv.scss'
 import live_video_icon from '../../assets/videocam-outline.svg'
 import live_video_icon2 from '../../assets/videocam-outline.svg'
-import  actvity from '../../assets/videocam-outline.svg'
+import actvity from '../../assets/videocam-outline.svg'
 import Avatar from '../../assets/Avatar.png'
 
 
-export  const PostArray=[
+export const PostArray = [
     {
-        postImage:live_video_icon,
-        imageDetails:'Live Video'
+        postImage: live_video_icon,
+        imageDetails: 'Live Video'
     },
     {
-        postImage:live_video_icon2,
-        imageDetails:'Images/Video'
+        postImage: live_video_icon2,
+        imageDetails: 'Images/Video'
     },
     {
-        postImage:actvity,
-        imageDetails:'Activity'
+        postImage: actvity,
+        imageDetails: 'Activity'
     }
 
-  ]
+]
 
 
 
 
 
-const PostDiv=()=>{
-  
+const PostDiv = ({ posts }) => {
 
 
-    return(
-       <div className="post-container">
+
+    return (
+        <div className="post-container">
             <div className='profile-pic-input'>
                 <img src={Avatar} alt="" />
                 <input type="text" placeholder='What is in your mind' />
             </div>
             <div className="actions">
                 {
-                    PostArray &&
-                        PostArray.map((item,index)=>{
-                            const{imageDetails,postImage}=item
-                                return (
-                                    <div className='live-video' key={index}>
-                                           <img src={postImage} alt="" />
-                                            <span>{imageDetails}</span>
-                                     </div> 
-                                )
-                        })
+                    posts &&
+                    posts.map((item, index) => {
+
+                        return (<>  <div className='live-video' key={index}>
+                            <p>{item.content} </p>
+                            <span>{item.id}</span>
+                            <div className='profile-pic-input'>
+
+
+                            </div>
+                        </div>
+
+
+                        </>
+
+
+
+                        )
+                    })
                 }
             </div>
-       </div>
+        </div>
     )
 }
 
--- src/pages/Timeline/Addpost.jsx
+++ src/pages/Timeline/Addpost.jsx
@@ -1,147 +1,67 @@
-import React from 'react'
-import Avatar from '../../assets/Avatar.png'
-import './Addpost.scss'
-import close from '../../assets/close.png'
-import { useSelector } from 'react-redux'
-import { getProfileError, getProfileStatus, getUserDetails, selectProfile } from '../Profile/ProfileSlice'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { createPost } from '../../features/Posts/PostSlice'
-// import { selectAllFriends } from '../../features/Friends/FriendsSlice'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Avatar from '../../assets/Avatar.png';
+import close from '../../assets/close.png';
+import { getUserDetails, selectProfile } from '../Profile/ProfileSlice';
+import { createPost } from '../../features/Posts/PostSlice';
+import './Addpost.scss';
 
 const Addpost = ({ closeEdit }) => {
-   const dispatch = useDispatch();
-   const profile = useSelector(selectProfile);
-   const status = useSelector(getProfileStatus);
-   const error = useSelector(getProfileError);
-   const user_id = localStorage.getItem('user_id');
-   const post_id = localStorage.getItem('post_id')
-   const [content, setContent] = useState('');
-   const [image, setImage] = useState('');
+  const dispatch = useDispatch();
+  const profile = useSelector(selectProfile);
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState('');
 
-   // const friends=useSelector(selectAllFriends)
-   // const profile=useSelector(selectProfile);
-   // console.log(profile)
+  useEffect(() => {
+    dispatch(getUserDetails(localStorage.getItem('user_id')));
+  }, [dispatch]);
 
-   console.log("user oif in the fetch", user_id)
+  const handlePostSubmit = async (e) => {
+    e.preventDefault();
+    console.log(image, content);
+    const response = await dispatch(createPost({ content, image }));
+    console.log("response of post creation", response);
+    // Handle response or errors here
+  };
 
-   useEffect(() => {
-      if (status === 'idle') {
-         dispatch(getUserDetails(user_id))
-         //  dispatch(getFriends())
-         console.log(profile)
-      }
-
-
- },[status, dispatch])
-
-
-
-
-
- const handlePostSubmit =async(e)=>{
-  e.preventDefault()
-
-  console.log(image,content)
-
- const response= await dispatch(createPost({content,image}));
- console.log("response of post creation",response)
-
- }
-   
- console.log(image,content)
   return (
-    <>
- <div className="container5">
- <div className="postholder">
-    <div className="header">
-    <div className="side-profile">
-          <img src={Avatar} alt="nopic" />
-          <div className="side-text">
-            <h4>Angela lee</h4>
-            <p>@angalee</p>
+    <div className="container5">
+      <div className="postholder">
+        <div className="header">
+          <div className="side-profile">
+            <img src={Avatar} alt="nopic" />
+            <div className="side-text">
+              <h4>Angela lee</h4>
+              <p>@angalee</p>
+            </div>
+          </div>
+          <div className="close" onClick={closeEdit}>
+            <img src={close} alt="close" />
           </div>
         </div>
-        <div className="close" onClick={closeEdit}>
-   <img src={close} />
-        </div>
-    </div>
-    <form action="" onSubmit={handlePostSubmit}>
-    <div className="textarea">
-        <input  placeholder='what do you want to talk about'
-           onChange={(e)=>{setContent(e.target.value)}}
-        
-        
-        
-        
-        />
-
-       
-        
-        </div>
-        <input type="text" placeholder='photo url'
-           onChange={(e)=>{setImage(e.target.value)}}
-        
-        />
-        <div className="footer">
-         <div className="btn">
-            <button type='submit'>Post</button>
-
-   }, [status, dispatch])
-
-   const handlePostSubmit = async (e) => {
-      e.preventDefault()
-
-      console.log(image, content)
-
-      const response = await dispatch(createPost({ content, image,post }));
-      console.log("response of post creation", response)
-
-   }
-   console.log(image, content)
-   return (
-      <>
-         <div className="container5">
-            <div className="postholder">
-               <div className="header">
-                  <div className="side-profile">
-                     <img src={Avatar} alt="nopic" />
-                     <div className="side-text">
-                        <h4>Angela lee</h4>
-                        <p>@angalee</p>
-                     </div>
-                  </div>
-                  <div className="close" onClick={closeEdit}>
-                     <img src={close} />
-                  </div>
-               </div>
-               <form action="" onSubmit={handlePostSubmit}>
-                  <div className="textarea">
-                     <input placeholder='what do you want to talk about'
-                        onChange={(e) => { setContent(e.target.value) }}
-
-
-                     />
-
-                  </div>
-                  <input type="text" placeholder='photo url'
-                     onChange={(e) => { setImage(e.target.value) }}
-
-                  />
-                  <div className="footer">
-                     <div className="btn">
-                        <button type='submit'>Post</button>
-                     </div>
-                  </div>
-               </form>
-
-
+        <form onSubmit={handlePostSubmit}>
+          <div className="textarea">
+            <input
+              placeholder="What do you want to talk about"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+          <input
+            type="text"
+            placeholder="Photo URL"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+          <div className="footer">
+            <div className="btn">
+              <button type="submit">Post</button>
             </div>
-         </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-      </>
-   )
-}
-
-export default Addpost
+export default Addpost;
