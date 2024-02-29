f8e373e0f715110056d85952f4d63b910b478a08
Stephen Ondieki <111267947+stevehotcodes@users.noreply.github.com>
2024-02-29 11:53:58 +0300

    Revert "worked on comments"

--- src/components/PostDiv/PostDiv.jsx
+++ src/components/PostDiv/PostDiv.jsx
@@ -2,64 +2,55 @@ import React from 'react'
 import '../../components/PostDiv/PostDiv.scss'
 import live_video_icon from '../../assets/videocam-outline.svg'
 import live_video_icon2 from '../../assets/videocam-outline.svg'
-import actvity from '../../assets/videocam-outline.svg'
+import  actvity from '../../assets/videocam-outline.svg'
 import Avatar from '../../assets/Avatar.png'
 
 
-export const PostArray = [
+export  const PostArray=[
     {
-        postImage: live_video_icon,
-        imageDetails: 'Live Video'
+        postImage:live_video_icon,
+        imageDetails:'Live Video'
     },
     {
-        postImage: live_video_icon2,
-        imageDetails: 'Images/Video'
+        postImage:live_video_icon2,
+        imageDetails:'Images/Video'
     },
     {
-        postImage: actvity,
-        imageDetails: 'Activity'
+        postImage:actvity,
+        imageDetails:'Activity'
     }
 
-]
+  ]
 
 
 
 
 
-const PostDiv = ({ posts }) => {
+const PostDiv=()=>{
+  
 
 
-
-    return (
-        <div className="post-container">
+    return(
+       <div className="post-container">
             <div className='profile-pic-input'>
                 <img src={Avatar} alt="" />
                 <input type="text" placeholder='What is in your mind' />
             </div>
             <div className="actions">
                 {
-                    posts &&
-                    posts.map((item, index) => {
-
-                        return (<>  <div className='live-video' key={index}>
-                            <p>{item.content} </p>
-                            <span>{item.id}</span>
-                            <div className='profile-pic-input'>
-
-
-                            </div>
-                        </div>
-
-
-                        </>
-
-
-
-                        )
-                    })
+                    PostArray &&
+                        PostArray.map((item,index)=>{
+                            const{imageDetails,postImage}=item
+                                return (
+                                    <div className='live-video' key={index}>
+                                           <img src={postImage} alt="" />
+                                            <span>{imageDetails}</span>
+                                     </div> 
+                                )
+                        })
                 }
             </div>
-        </div>
+       </div>
     )
 }
 
--- src/features/comments/commentSlice.js
+++ src/features/comments/commentSlice.js
@@ -1,38 +1,78 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const comment_API = ` http://localhost:3000/comment`;
+const initialState = {
+  comments: [],
+  status: "idle", // loading | succeeded | failed | error
+  error: null,
+};
 
-const BASE_URL = 'http://localhost:3000/';
-
-
-const token = localStorage.getItem('token')
-export const commentSlice = createApi({
-  reducerPath: 'commentsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
-    prepareHeaders: (headers, { getState }) => {
-      // Retrieve token from localStorage
-   
-
-      // If token exists, set Authorization header
-      if (token) {
-        headers.set('Authorization', ` ${token}`);
-      }
-
-      return headers;
-    },
-  }),
-  endpoints: (builder) => ({
-    addComment: builder.mutation({
-      query: ({ post_id, comment }) => ({
-        url: `/comment/${post_id}`,
-        headers:{
-          Authorization: `${token}`
-        },
-        method: 'POST',
-        body: { comment }
-      }),
-    }),
-  }),
+export const createcomment = createAsyncThunk("comment/create", async ({content}) => {
+  try {
+    const token = localStorage.getItem('token');
+    console.log(token)
+    const config = {
+        headers: {
+            Authorization:`${token}`
+        }
+    };
+    
+    const response = await axios.post(comment_API, {content},config);
+    console.log(response, "response from api")
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
 });
+// const comment_API = ` http://localhost:3000/api/comment`;
+export const getcomments = createAsyncThunk(
+  "comment/get",
+  async()=>{
+    const token = localStorage.getItem('token')
+    const config={
+      headers:{
+        Authorization:`${token}`
+      }
+    }
+    const response= await axios.get(comment_API,config);
+     console.log(response)
+    return response.data
+  }
+);
 
-export const { useAddCommentMutation } = commentSlice;
+export const commentSlice = createSlice({
+  name: "comments",
+  initialState ,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(createcomment.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(createcomment.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.comments = action.payload;
+      })
+      .addCase(createcomment.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(getcomments.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getcomments.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.comments = action.payload;
+      })
+      .addCase(getcomments.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+export const selectcomments = (state) => state.comments.comments;
+export const getcommentStatus = (state) => state.comments.status;
+export const getcommentError = (state) => state.comments.error;
+
+export default commentSlice.reducer;
