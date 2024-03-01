635ab354976c1ef766ca50a80bce1b0aaca34d35
simon kamau <https://github.com/SIMONKAMAU21/own-git-.git>
2024-03-01 05:01:53 -0800

    (modified) the store

--- src/app/store.js
+++ src/app/store.js
@@ -5,9 +5,9 @@ import loginReducer from '../pages/Login/AuthenticationSlice';
 import friendsReducer from '../features/Friends/FriendsSlice';
 import registrationReducer from '../features/Register/registerSlice';
 import profileReducer from '../pages/Profile/ProfileSlice';
-import messageReducer from '../components/MessagesComponent/messageSlice';
 import postReducer from '../features/Posts/PostSlice';
 import { commentSlice } from "../features/comments/commentSlice";
+import { EventApi } from "../features/Events/EventsSlice";
 
 export const store = configureStore({
     reducer: {
@@ -16,13 +16,17 @@ export const store = configureStore({
         authentication: loginReducer,
         registration: registrationReducer,
         profile: profileReducer,
-        message: messageReducer,
         posts: postReducer,
         [commentSlice.reducerPath]: commentSlice.reducer, // Use reducerPath as the key
+        [EventApi.reducerPath]:EventApi.reducer
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(commentSlice.middleware), // Add RTK-Query middleware
+    getDefaultMiddleware().concat(commentSlice.middleware), // Add RTK-Query middleware
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(EventApi.middleware), // Add RTK-Query middleware
+
 });
+
 
 // Setup RTK-Query listeners
 setupListeners(store.dispatch);
